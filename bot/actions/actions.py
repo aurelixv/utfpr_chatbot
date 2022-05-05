193b331231d1864030e23f1edc94479215d92053
Aurelio <aurelio@alunos.utfpr.edu.br>
2022-05-05 01:24:34 -0300

    v0.13 - nlu and custom action improvements

--- bot/actions/actions.py
+++ bot/actions/actions.py
@@ -58,49 +58,65 @@ class ActionGetSchedule(Action):
         """
         Function that receives a campus (key), pre-processes it and returns its values
         """
-        if CONN:
+        global CONN
+        # connects to db if expired
+        if CONN.closed != 0:
+            print('--- Reconnecting to Postgres... ---')
+            CONN = psql_connect()
+
+        if CONN.closed == 0:
             # pre-process the campus parameter
             campus = unidecode(campus.upper())
 
             # query the data
-            query = """
-            SELECT B.CAMPUS_NAME,
-                C.PHASE_NAME,
-                A.YEAR,
-                A.SEMESTER,
-                A.START_TIMESTAMP,
-                A.END_TIMESTAMP
-            FROM ENROLLMENT_SCHEDULE A
-            LEFT JOIN CAMPI B
-                ON A.CAMPUS_ID = B.CAMPUS_ID
-            LEFT JOIN PHASE C
-                ON A.PHASE_ID = C.PHASE_ID
-            WHERE B.CAMPUS_NAME = (%s)
-                AND CONCAT(A.YEAR, A.SEMESTER) = 
-                    (
-                        SELECT CONCAT(A.YEAR, A.SEMESTER) 
-                        FROM ENROLLMENT_SCHEDULE 
-                        ORDER BY YEAR DESC, SEMESTER DESC 
-                        LIMIT 1
-                    )
+            q_year_semester = """
+                SELECT A.YEAR,
+                    A.SEMESTER
+                FROM ENROLLMENT_SCHEDULE A
+                LEFT JOIN CAMPI B
+                    ON A.CAMPUS_ID = B.CAMPUS_ID
+                WHERE B.CAMPUS_NAME = (%s)
+                ORDER BY YEAR DESC, SEMESTER DESC 
+                LIMIT 1
+            """
+            q_schedule = """
+                SELECT B.CAMPUS_NAME,
+                    C.PHASE_NAME,
+                    A.START_TIMESTAMP,
+                    A.END_TIMESTAMP
+                FROM ENROLLMENT_SCHEDULE A
+                LEFT JOIN CAMPI B
+                    ON A.CAMPUS_ID = B.CAMPUS_ID
+                LEFT JOIN PHASE C
+                    ON A.PHASE_ID = C.PHASE_ID
+                WHERE B.CAMPUS_NAME = (%s)
+                    AND A.YEAR = (%s)
+                    AND A.SEMESTER = (%s)
             """
             with CONN.cursor() as cur:
-                cur.execute(query, (campus,))
-                data = cur.fetchall()
+                cur.execute(q_year_semester, (campus,))
+                year_semester = cur.fetchall()
+                if len(year_semester) > 0:
+                    year = year_semester[0][0]
+                    semester = year_semester[0][1]
+                    cur.execute(q_schedule, (campus, year, semester))
+                    data = cur.fetchall()
+                else:
+                    return None
 
             if len(data) > 0:
                 schedule = {
                     'campus': data[0][0].title(),
-                    'year': data[0][2],
-                    'semester': data[0][3],
+                    'year': year,
+                    'semester': semester,
                     'phases': {}
                 }
                 for line in data:
                     schedule['phases'][line[1].lower()] = {
-                        'start_date': line[4].strftime("%d/%m/%Y"), 
-                        'start_hour': line[4].strftime("%H"),
-                        'end_date': line[5].strftime("%d/%m/%Y"),
-                        'end_hour': line[5].strftime("%H")
+                        'start_date': line[2].strftime("%d/%m/%Y"),
+                        'start_hour': line[2].strftime("%H"),
+                        'end_date': line[3].strftime("%d/%m/%Y"),
+                        'end_hour': line[3].strftime("%H")
                     }
                 return schedule
 
@@ -116,18 +132,21 @@ class ActionGetSchedule(Action):
         if schedule:
 
             response = f"Matrícula de veteranos do {schedule['semester']}o semestre "\
-                + f"de {schedule['year']} para o campus {schedule['campus']}:\n\n"
+                f"de {schedule['year']} para o campus {schedule['campus']}:\n\n"
 
             phases = schedule['phases']
 
             for phase in phases:
                 response += f"{phase.upper()}\n"\
-                    f"- Início: {phases[phase]['start_date']} a partir das {phases[phase]['start_hour']} horas.\n" \
-                    + f"- Término: {phases[phase]['end_date']} até às {phases[phase]['end_hour']} horas.\n\n"
+                    f"- Início: {phases[phase]['start_date']} a partir das "\
+                    f"{phases[phase]['start_hour']} horas.\n" \
+                    f"- Término: {phases[phase]['end_date']} até às "\
+                    f"{phases[phase]['end_hour']} horas.\n\n"
 
             dispatcher.utter_message(text=response)
         else:
-            dispatcher.utter_message(text=f"Me desculpe, mas não consegui localizar o cronograma de matrícula de veteranos para o campus {campus.title()}.")
+            dispatcher.utter_message(text="Me desculpe, mas não consegui localizar o "\
+                f"cronograma de matrícula de veteranos para o campus {campus.title()}.")
 
         # clears the campus slot
         return [SlotSet('campus', None)]
